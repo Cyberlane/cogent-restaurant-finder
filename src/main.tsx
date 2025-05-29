fe85ae12c43acf340ce2de268f3670d695835cf3
Justin Nel <justin@cyber-lane.com>
2025-05-29 23:10:02 +0900

    chore: tweak the devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 
+import './styles/tailwind.css';
 import './common/i18n';
-
+import { isProduction } from './common/utils';
 import { routeTree } from './routeTree.gen';
 
 const router = createRouter({ routeTree });
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
 
 export type TanstackRouter = typeof router;
 
@@ -17,13 +28,33 @@ declare module "@tanstack/react-router" {
   }
 }
 
+const TanstackRouterDevtools = isProduction
+  ? () => null
+  : React.lazy(() => import('@tanstack/react-router-devtools').then((result) => ({
+    default: result.TanStackRouterDevtools,
+  })));
+
+const ReactQueryDevtools = isProduction
+  ? () => null
+  : React.lazy(() => import('@tanstack/react-query-devtools').then((result) => ({
+    default: result.ReactQueryDevtools
+  })));
+
 const rootElement = document.querySelector('#root') as Element;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
       <React.Suspense fallback={<div>Loading...</div>}>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <TanstackRouterDevtools
+            initialIsOpen={false}
+            position="bottom-left"
+            router={router}
+          />
+          <ReactQueryDevtools initialIsOpen={false} position="bottom" />
+        </QueryClientProvider>
       </React.Suspense>
     </React.StrictMode>
   )
