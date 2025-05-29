66080dcff00bf2ff0b296314ebc8953c2a16fa7c
Justin Nel <justin@cyber-lane.com>
2025-05-30 00:00:23 +0900

    fix: tailwind was not working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
-export default {
+import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
   content: ["./index.html", "./src/**/*,.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+});
