959305bd2df17c23bd7e6199910286bc0c6c0a22
Justin Nel <justin@cyber-lane.com>
2025-06-01 22:10:41 +0900

    chore: fieldsets to try group filter and sort

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,12 @@
-import { Container, Group, Loader, Tabs } from '@mantine/core';
+import {
+  Card,
+  Container,
+  Fieldset,
+  Group,
+  SimpleGrid,
+  Skeleton,
+  Tabs,
+} from '@mantine/core';
 import { useTranslation } from 'react-i18next';
 
 import Filters from '../components/Filters/Filters';
@@ -9,36 +17,57 @@ import Results from '../components/SearchResults';
 import Sort from '../components/Sort';
 import { useRestaurants } from '../hooks/useRestaurants';
 
+const ListSkeleton = () => {
+  return (
+    <SimpleGrid cols={{ base: 1, sm: 2, md: 3 }} spacing="lg">
+      <Card>
+        <Skeleton height={50} />
+        <Skeleton height={20} mt={10} />
+        <Skeleton height={160} mt={10} />
+      </Card>
+      <Card>
+        <Skeleton height={50} />
+        <Skeleton height={20} mt={10} />
+        <Skeleton height={160} mt={10} />
+      </Card>
+    </SimpleGrid>
+  );
+};
+
 const HomePage = () => {
   const { t } = useTranslation();
   const { data: restaurants, isLoading } = useRestaurants();
 
   return (
     <Container size="xl" px="md" py="xl">
-      <Group justify="space-between" wrap="nowrap">
-        <SearchBar />
-        <RandomButton />
-      </Group>
-
-      <Filters />
-      <Sort />
-
-      {isLoading || restaurants == null ? (
-        <Loader />
-      ) : (
-        <Tabs mt="lg" defaultValue="list" keepMounted={false}>
-          <Tabs.List>
-            <Tabs.Tab value="list">{t('home.list')}</Tabs.Tab>
-            <Tabs.Tab value="map">{t('home.map')}</Tabs.Tab>
-          </Tabs.List>
-          <Tabs.Panel value="list" pt="xs">
+      <Fieldset legend="Search">
+        <Group justify="space-between" wrap="nowrap">
+          <SearchBar />
+          <RandomButton />
+        </Group>
+      </Fieldset>
+      <Fieldset legend="Filters">
+        <Filters />
+      </Fieldset>
+      <Fieldset legend="Sort">
+        <Sort />
+      </Fieldset>
+      <Tabs mt="lg" defaultValue="list" keepMounted={false}>
+        <Tabs.List>
+          <Tabs.Tab value="list">{t('home.list')}</Tabs.Tab>
+          <Tabs.Tab value="map">{t('home.map')}</Tabs.Tab>
+        </Tabs.List>
+        <Tabs.Panel value="list" pt="xs">
+          {isLoading || restaurants == null ? (
+            <ListSkeleton />
+          ) : (
             <Results results={restaurants} />
-          </Tabs.Panel>
-          <Tabs.Panel value="map" pt="xs">
-            <RestaurantListMap restaurants={restaurants.results ?? []} />
-          </Tabs.Panel>
-        </Tabs>
-      )}
+          )}
+        </Tabs.Panel>
+        <Tabs.Panel value="map" pt="xs">
+          <RestaurantListMap restaurants={restaurants?.results ?? []} />
+        </Tabs.Panel>
+      </Tabs>
     </Container>
   );
 };
